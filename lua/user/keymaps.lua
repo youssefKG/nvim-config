ecdad53edad4a3e7e54d0e79342f32c25366d4f3
youssef <yousseftaoussi894@gmail.com>
2024-01-03 10:44:01 +0100

    commit nvim config

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,28 @@
+local opts = {noremap = true , silent= true}
+local keymap = vim.api.nvim_set_keymap
+--make space as leader key 
+keymap("" , "<space>" , "<Nop>" , opts)
+vim.g.mapleader = " "
+-- windiow navitation 
+vim.api.nvim_set_keymap("n" , "<C-h>" , "<C-w>h" , opts) 
+keymap("n" , "<C-j>" , "<C-w>j" , opts)
+keymap("n" , "<C-k>" , "<C-w>k", opts)
+keymap("n" , "<C-l" , "<C-w>l", opts ) 
+keymap("n" , "<leader>e", ":Lex 20<CR>" , opts ) 
+-- resize window 
+keymap("n" ,"<C-left>" , ":vertical resize +2<CR>" , opts) 
+keymap("n" , "<C-right>" , ":vertical resize -2<CR>" , opts) 
+keymap("n" , "<C-up>" , ":resize +2<CR>", opts) 
+keymap("n" , "<C-down>", ":resize -2<CR>" , opts)
+--navigate between buffers 
+keymap("n" , "<S-l>" , ":bn <CR>" , opts)
+keymap("n" , "<S-h>" , ":bp <CR>", opts)
+keymap("n" , "<leader>bb" , ":buffers <CR>" , opts) 
+keymap("n" , "<leader>bd" , ":bdelete <CR>" , opts ) 
+
+--insert mode 
+keymap("i" , "<leader><leader>", "<Esc>" , opts) 
+
+ 
+
+
---
+++ lua/user/options.lua
@@ -0,0 +1,8 @@
+local opts = vim.opt 
+opts.relativenumber = true 
+opts.cursorline = true 
+opts.mouse = a
+opts.autoindent = true 
+opts.tabstop =1 
+opts.shiftwidth =1 
+opts.number = true 
