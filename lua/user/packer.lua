4fb86c9c2062daee2bd14473651ef03092fa3b69
youssef <yousseftaoussi894@gmail.com>
2024-01-04 16:19:03 +0100

    add eslint config

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -23,20 +23,20 @@ return require("packer").startup(function(use)
 		branch = "v1.x",
 		requires = {
 			-- LSP Support
-			{ "neovim/nvim-lspconfig" },             -- Required
-			{ "williamboman/mason.nvim" },           -- Optional
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
 			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
 			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },         -- Required
-			{ "hrsh7th/cmp-nvim-lsp" },     -- Required
-			{ "hrsh7th/cmp-buffer" },       -- Optional
-			{ "hrsh7th/cmp-path" },         -- Optional
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-buffer" }, -- Optional
+			{ "hrsh7th/cmp-path" }, -- Optional
 			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
-			{ "hrsh7th/cmp-nvim-lua" },     -- Optional
+			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
 
 			-- Snippets
-			{ "L3MON4D3/LuaSnip" },             -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
 			{ "rafamadriz/friendly-snippets" }, -- Optional
 		},
 	})
@@ -59,4 +59,5 @@ return require("packer").startup(function(use)
 			require("toggleterm").setup()
 		end,
 	})
+	use("p00f/nvim-ts-rainbow")
 end)
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,20 +1,28 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c","javascript", "lua", "vim", "vimdoc", "query" },
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all" (the five listed parsers should always be installed)
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
 
-  -- List of parsers to ignore installing (or "all")
+	-- List of parsers to ignore installing (or "all")
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-  highlight = {
-    enable = true,
-  },
-}
+	highlight = {
+		enable = true,
+	},
+	rainbow = {
+		enable = true,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+})
